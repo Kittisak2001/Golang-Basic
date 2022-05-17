269789aae0e54061ccfefebd1ed92243628b5100
Kittisak Phatchaiphongsa <kittisak.ph2001@gmail.com>
2022-05-17 17:40:34 +0700

    Learn Golang Basic

---
+++ calculator/app.go
@@ -0,0 +1,9 @@
+package calculator
+
+func Add(number1, number2 int) int {
+	return number1 + number2
+}
+
+func Sub(number1, number2 int) int {
+	return number1 - number2
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(calcilator.Add(50, 100))
+	fmt.Println(calcilator.Sub(50, 100))
+}
